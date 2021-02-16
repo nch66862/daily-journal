bd5ce16c759b437b3cba4a871e29ebb4b357246a
Nick Carver <nicholascarver2@outlook.com>
2021-02-16 10:21:05 -0600

    added a function to delete journal entries. Added functionality to deletean entry.

--- scripts/entries/JournalEntry.js
+++ scripts/entries/JournalEntry.js
@@ -1,3 +1,5 @@
+import { deleteJournalEntry } from "./JournalDataProvider.js"
+
 export const JournalEntryComponent = (entry) => {
     return `
         <section id="entry--${entry.id}" class="journalEntry">
@@ -11,29 +13,15 @@ export const JournalEntryComponent = (entry) => {
             <p>${entry.plans}</p>
             <p>mood:</p>
             <p>${entry.mood.label}</p>
-            <button id="deleteEntryButton---${entry.id}">Delete</button>     
+            <button id="deleteEntryButton--${entry.id}">Delete</button>     
         </section>
             `
-        }
+}
 
 const eventHub = document.querySelector(".container") //define what the eventHub will be (needs to be the same across the application)
 eventHub.addEventListener("click", clickEvent => {
-    if (clickEvent.target.id.startsWith("deleteEntryButton")) {
-        clickEvent.preventDefault()
-        // Make a new object representation of a note
-        const newEntry = {
-            date: document.getElementById("entry-date").value,
-            concept: document.getElementById("entry-concept").value,
-            learned: document.getElementById("entry-learned").value,
-            plans: document.getElementById("entry-plans").value,
-            moodId: parseInt(document.getElementById("entry-mood").value)
-        }
-        // Change API state and application state
-        saveJournalEntry(newEntry)
-        document.getElementById("entry-date").value = ""
-        document.getElementById("entry-concept").value = ""
-        document.getElementById("entry-learned").value = ""
-        document.getElementById("entry-plans").value = ""
-        document.getElementById("entry-mood").value = ""
-}
+    if (clickEvent.target.id.startsWith("deleteEntryButton--")) {
+        const [prefix, noteID] = clickEvent.target.id.split("--")
+        deleteJournalEntry(noteID)
+    }
 })

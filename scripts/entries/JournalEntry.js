f91ddebb9e9ca860b211262c65da10f340fd6702
Nick Carver <nicholascarver2@outlook.com>
2021-02-16 10:11:27 -0600

    added delete button to all entries. Started on event listener

--- scripts/entries/JournalEntry.js
+++ scripts/entries/JournalEntry.js
@@ -10,7 +10,30 @@ export const JournalEntryComponent = (entry) => {
             <p>my plans:</p>
             <p>${entry.plans}</p>
             <p>mood:</p>
-            <p>${entry.mood.label}</p>        
+            <p>${entry.mood.label}</p>
+            <button id="deleteEntryButton---${entry.id}">Delete</button>     
         </section>
             `
         }
+
+const eventHub = document.querySelector(".container") //define what the eventHub will be (needs to be the same across the application)
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id.startsWith("deleteEntryButton")) {
+        clickEvent.preventDefault()
+        // Make a new object representation of a note
+        const newEntry = {
+            date: document.getElementById("entry-date").value,
+            concept: document.getElementById("entry-concept").value,
+            learned: document.getElementById("entry-learned").value,
+            plans: document.getElementById("entry-plans").value,
+            moodId: parseInt(document.getElementById("entry-mood").value)
+        }
+        // Change API state and application state
+        saveJournalEntry(newEntry)
+        document.getElementById("entry-date").value = ""
+        document.getElementById("entry-concept").value = ""
+        document.getElementById("entry-learned").value = ""
+        document.getElementById("entry-plans").value = ""
+        document.getElementById("entry-mood").value = ""
+}
+})
